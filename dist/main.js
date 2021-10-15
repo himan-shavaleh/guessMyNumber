79843d2bd78687291d25c87f7ea072455f827a28
himan shavaleh <88880060+himan-shavaleh@users.noreply.github.com>
2021-10-15 21:12:37 +0330

    refactoring the app and making it cleaner

--- dist/main.js
+++ dist/main.js
@@ -1,47 +1,48 @@
-// define variables
-const min = document.querySelector('.min');
-const max = document.querySelector('.max');
-const checkBotton = document.querySelector('#check');
-const againButton = document.querySelector('#again');
+// another way to write this app {cleaner and better way}
+
+// defining variables
+const check = document.querySelector('#check');
+const question = document.querySelector('.question');
+const again = document.querySelector('#again');
+let score = 20;
 let highScore = 0;
-let point = 20;
-const inputGuess = document.querySelector('#input-guess');
-max.innerHTML=20;
-min.innerHTML = 1;
-// const rand = Math.trunc((Math.random()*Number(max.innerHTML))+Number(min.innerHTML));
-const rand = 2
-checkBotton.addEventListener('click',checkGuess);
-againButton.addEventListener('click',function(){
-    point = 20;
-    document.querySelector('body').style.background = '#333';
-    document.querySelector('.message').innerHTML = 'start guessing ...';
-    document.querySelector('.score').innerHTML = 20;
-})
+let secretNumber = Math.trunc((Math.random()*20)+1);
+// events
+check.addEventListener('click',checkGuess);
+again.addEventListener('click',againGame);
+// funtions
 
-
-function checkGuess(){
-    let guess = Number(inputGuess.value);
-    if(guess === rand){
-        if(point >= highScore){
-            highScore = point;
-        }
-        document.querySelector('.highscore').innerHTML = highScore;
-        document.querySelector('.question').innerHTML = rand;
-        document.querySelector('.message').innerHTML = 'correct you win';
-        winFunc();
-    }else if(guess>rand){
-        document.querySelector('.message').innerHTML = 'too high';
-        point --;
-        document.querySelector('.score').innerHTML = point;
-
-    }else{
-        document.querySelector('.message').innerHTML = 'too low';
-        point--;
-        document.querySelector('.score').innerHTML = point;
-    }
-
+// a function to show message
+const showMessage = (message)=>{
+    document.querySelector('.message').innerHTML = message;
 }
-
-function winFunc(){
-    document.querySelector('body').style.background = 'green';
+// to preduce the game
+function checkGuess(){
+const num = Number(document.querySelector('#input-guess').value);
+if (!num){
+showMessage('No Numbewr🔴');
+}else{
+if(num === secretNumber){
+    showMessage('you Win , Corect✅');
+    question.innerHTML = secretNumber;
+    document.querySelector('body').style.background = '#60fe21';
+    if(highScore<score){
+        highScore = score;
+        document.querySelector('.highscore').innerHTML = highScore;
+    }
+}else{
+    score--;
+    document.querySelector('.score').innerHTML = score;
+    num>secretNumber?showMessage('too high ⬆'):showMessage('too low ⬇');
+}
+}
+}
+// again function
+function againGame(){
+    document.querySelector('body').style.background = '#333';
+    score = 20;
+    document.querySelector('.score').innerHTML = 20;
+    question.innerHTML = '?';
+    showMessage('start Guessing ...');
+    secretNumber = Math.trunc((Math.random()*20)+1);
 }
